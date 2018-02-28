9997c8e7f5fea535c757cb189cb6d0d62d4b4c1a
Andreas Molzer <andreas.molzer@gmx.de>
2018-03-01 00:53:30 +0100

    Int formatting with the speed test

--- test/speed.cpp
+++ test/speed.cpp
@@ -9,7 +9,7 @@ int main() {
 	constexpr auto formatter = mould::compile<format>();
 
 	for(int i = 0; i < 2000000; i++) {
-		std::cout << mould::format(formatter, "world");
+		std::cout << mould::format(formatter, 42);
 /* Format equivalent to "%0.10f:%04d:%+g:%s:%p:%c:%%\n" */
 	}
 }
