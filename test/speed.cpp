51a32dfd17efa1882217da056af8fa524cad827e
Andreas Molzer <andreas.molzer@gmx.de>
2018-03-03 01:33:48 +0100

    Specify the full speed test string

--- test/speed.cpp
+++ test/speed.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cpp_mould.hpp>
 
-static constexpr char format[] = "Hello {}\n";
+static constexpr char format[] = "{:0.10f}:{:04d}:{:+}:{:s}:{:p}:{:c}:%\n";
 
 int main() {
 	std::ios::sync_with_stdio(false);
@@ -9,7 +9,8 @@ int main() {
 	constexpr auto formatter = mould::compile<format>();
 
 	for(int i = 0; i < 2000000; i++) {
-		std::cout << mould::format(formatter, 42);
-/* Format equivalent to "%0.10f:%04d:%+g:%s:%p:%c:%%\n" */
+		/* Format equivalent to "%0.10f:%04d:%+g:%s:%p:%c:%%\n" */
+		std::cout << mould::format(formatter,
+			1.234, 42, 3.13, "str", (void*)1000, 'X');
 	}
 }
