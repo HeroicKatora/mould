84f95232222fa1a123df7ef2c8310388ca708c4a
Andreas Molzer <andreas.molzer@gmx.de>
2018-03-06 00:02:38 +0100

    Constexpr construct resolved formatting objects and dynamically bind arguments

--- include/cpp_mould/constexpr_driver.hpp
+++ include/cpp_mould/constexpr_driver.hpp
@@ -2,8 +2,12 @@
 #define CPP_MOULD_CONSTEXPR_DRIVER_HPP
 #include <tuple>
 #include "argument.hpp"
+#include "engine.hpp"
+#include "format.hpp"
+#include "format_info.hpp"
 
 namespace mould::internal::constexpr_driver {
+  /* Resolved expression representation */
   template<size_t N>
   struct ExpressionInformation {
     int indices[N];
@@ -15,7 +19,7 @@ namespace mould::internal::constexpr_driver {
     FormattingResult (*function)(const T&, Formatter);
 
     constexpr TypedArgumentExpression initialize(FullOperation operation) {
-      return { argument_index, nullptr };
+      return { argument_index, TypedFormatter<T>::automatic };
     }
   };
 
@@ -23,7 +27,7 @@ namespace mould::internal::constexpr_driver {
     size_t offset, length;
 
     constexpr LiteralExpression initialize(FullOperation operation) {
-      return { offset, length };
+      return { operation.literal.offset, operation.literal.length };
     }
   };
 
@@ -50,6 +54,7 @@ namespace mould::internal::constexpr_driver {
   template<typename T>
   constexpr auto ExpressionData = _expression_data<T>();
 
+  /* Compile the useful data, retrieve the specific formatting functions */
   template<int Index, typename ArgsTuple>
   constexpr auto uninitialized_expression() {
     if constexpr(Index < 0) {
@@ -68,6 +73,9 @@ namespace mould::internal::constexpr_driver {
         { expressions ... }
       };
     }
+
+    template<size_t index>
+    using ExpressionType = typename std::tuple_element<index, std::tuple<E...>>::type;
   };
 
   template<typename T, typename ... E>
@@ -86,6 +94,70 @@ namespace mould::internal::constexpr_driver {
   template<typename Format, typename ... Arguments>
   constexpr auto CompiledExpressions = build_expressions<Format, std::tuple<Arguments...>>(
     std::make_index_sequence<std::size(Format::data.code)>{});
+
+  /* Run the engine with arguments */
+  struct ExpressionContext {
+    Engine& engine;
+    Buffer<const char> format_buffer;
+  };
+
+  template<typename ExpT>
+  struct Eval;
+
+  template<>
+  struct Eval<LiteralExpression> {
+    template<typename Format, size_t index, typename ... Arguments>
+    static inline auto evaluate(
+      const ExpressionContext& context,
+      const Arguments& ... args) 
+    {
+      constexpr auto& expression = std::get<index>(CompiledExpressions<Format, Arguments...>.expressions);
+      context.engine.append(
+        context.format_buffer.begin() + expression.offset, 
+        context.format_buffer.begin() + expression.offset + expression.length);
+    }
+  };
+
+  template<typename T>
+  struct Eval<TypedArgumentExpression<T>> {
+    template<typename Format, size_t index, typename ... Arguments>
+    static inline auto evaluate(
+      const ExpressionContext& context,
+      const Arguments& ... args)
+    {
+      constexpr auto& expression = std::get<index>(CompiledExpressions<Format, Arguments...>.expressions);
+      constexpr auto fn = expression.function;
+      const auto& argument = std::get<ExpressionData<Format>.indices[index]>(std::tie(args...));
+      ::mould::Format format {
+        0, 0, 0, Alignment::Default, Sign::Default
+      };
+      fn(argument, ::mould::Formatter{context.engine, format});
+    }
+  };
+
+  struct Ignore {
+    template<typename ... I>
+    Ignore(I&& ...) {}
+  };
+
+  template<typename Format, typename ... Arguments, size_t ... Indices>
+  inline auto _eval(ExpressionContext context, std::index_sequence<Indices...>, const Arguments& ... args) {
+    using Compiled = decltype(CompiledExpressions<Format, Arguments...>);
+    Ignore ignore{(Eval<typename Compiled::template ExpressionType<Indices>>::template evaluate<Format, Indices>(
+      context, args...), 0) ...};
+  }
+
+  template<typename Format, typename ... Arguments>
+  inline auto eval(Engine engine, const Arguments& ... args) {
+    ExpressionContext context {
+      engine,
+      Format::data.format_buffer()
+    };
+    return _eval<Format>(
+      context,
+      std::make_index_sequence<std::size(Format::data.code)>{},
+      args... );
+  }
 }
 
 namespace mould {
@@ -95,12 +167,13 @@ namespace mould {
     Arguments&&... arguments)
   {
     using namespace internal::constexpr_driver;
-    constexpr auto expressions = CompiledExpressions<Format, Arguments...>;
-    std::stringstream output;
 
-    // for(auto index: arg_indices.indices) output << index;
+    std::string output;
+    internal::Engine engine{output};
 
-    return output.str();
+    eval<Format>(engine, arguments...);
+
+    return output;
   }
 }
 
