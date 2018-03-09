5f937fb468b19a595e6725bd16aa9336a6c51b0d
Andreas Molzer <andreas.molzer@gmx.de>
2018-03-09 22:15:42 +0100

    Better support for integer width padding

--- include/cpp_mould/arguments/int.hpp
+++ include/cpp_mould/arguments/int.hpp
@@ -1,6 +1,6 @@
 #ifndef CPP_MOULD_ARGUMENTS_INT_HPP
 #define CPP_MOULD_ARGUMENTS_INT_HPP
-#include <climits>
+#include <limits>
 
 #include "../argument.hpp"
 
@@ -18,37 +18,43 @@ namespace mould {
       return FormattingResult::Success;
     }
 
+    char view[std::numeric_limits<int>::digits10] = {};
+
     int pre_length = (pvalue < 0 || formatter.format().sign != internal::Sign::Default) ? 1 : 0;
     // This convoluted mess avoids the failure on -MAX_INT
     const unsigned value = (pvalue < 0) ? (~static_cast<unsigned>(pvalue)) + static_cast<unsigned>(1) : pvalue;
 
-    static_assert(~static_cast<unsigned>(INT_MIN) + 1 == 2147483648u);
+    static_assert(~static_cast<unsigned>(std::numeric_limits<int>::min()) + 1 == 2147483648u);
     constexpr static unsigned comparisons[] = // -INT_MIN = 2147483648
       {10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000 };
 
-    int val_length = 1;
+    int number_length = 1;
     for(auto cmp : comparisons) {
-      val_length += (value >= cmp) ? 1 : 0;
+      number_length += (value >= cmp) ? 1 : 0;
     }
-    val_length = std::max(val_length, (int) formatter.format().width);
+    const unsigned int formatted_length = number_length;
 
-    const auto view = formatter.reserve(pre_length + val_length);
-
+    int remaining_length = formatter.format().width - number_length;
     unsigned iterval = value;
+
     for(;;) {
-      if(iterval > 0) view[pre_length + val_length - 1] = '0' + (iterval % 10);
-      if(iterval > 10) view[pre_length + val_length - 2] = '0' + ((iterval/10) % 10);
-      if(iterval > 100) view[pre_length + val_length - 3] = '0' + ((iterval/100) % 10);
-      if(iterval > 1000) view[pre_length + val_length - 4] = '0' + ((iterval/1000) % 10);
+      if(iterval > 0) view[number_length - 1] = '0' + (iterval % 10);
+      if(iterval > 10) view[number_length - 2] = '0' + ((iterval/10) % 10);
+      if(iterval > 100) view[number_length - 3] = '0' + ((iterval/100) % 10);
+      if(iterval > 1000) view[number_length - 4] = '0' + ((iterval/1000) % 10);
       iterval /= 10000;
-      val_length -= 4;
-      if(val_length <= 0) break;
+      number_length -= 4;
+      if(number_length <= 0) break;
     }
 
-    if(pvalue < 0) view[0] = '-';
-    else if(formatter.format().sign == internal::Sign::Always) view[0] = '+';
-    else if(formatter.format().sign == internal::Sign::Pad) view[0] = ' ';
+    if(pvalue < 0) formatter.append('-');
+    else if(formatter.format().sign == internal::Sign::Always) formatter.append('+');
+    else if(formatter.format().sign == internal::Sign::Pad) formatter.append(' ');
 
+    const char padding = formatter.format().has_padding ? (char) formatter.format().padding : ' ';
+    for(int i = 0; i < remaining_length; i++) formatter.append(padding);
+
+    formatter.append(std::string_view{view, formatted_length});
     return FormattingResult::Success;
   }
 }
