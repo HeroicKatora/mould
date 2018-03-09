7c22942828a4fa62b572219d9da907cc65219e48
Andreas Molzer <andreas.molzer@gmx.de>
2018-03-09 02:43:10 +0100

    Naively shorten the pointer result

--- include/cpp_mould/arguments/pointer.hpp
+++ include/cpp_mould/arguments/pointer.hpp
@@ -10,18 +10,23 @@ namespace mould {
 
   template<typename Formatter>
   FormattingResult format_pointer(void* ptr, Formatter formatter) {
-    auto buffer = formatter.reserve(sizeof(void*)*2 + 2);
+    char buffer[sizeof(void*)*2 + 3] = {};
 
-    buffer[0] = '0';
-    buffer[1] = 'x';
     size_t asint = (size_t) ptr;
     size_t shft = sizeof(void*)*8 -4;
     
+    int begin_index = 2*sizeof(void*) + 3;
     for(int i = 0; i < 2*sizeof(void*); i++) {
         auto chr = ((asint >> shft) & 0xF);
+        if(chr != 0) begin_index = std::min(begin_index, i);
         buffer[2+i] = chr < 10 ? '0' + chr : 'a' + chr - 10;
         shft -= 4;
     }
+
+    buffer[begin_index] = '0';
+    buffer[begin_index + 1] = 'x';
+
+    formatter.append(buffer + begin_index);
     
     return FormattingResult::Success;
   }
