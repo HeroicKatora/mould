3a5d8f7f77670beffb617fcee10d83be5b4d924c
Andreas Molzer <andreas.molzer@gmx.de>
2018-07-27 21:11:35 +0200

    Preliminary formatting with ryu
    Ryu currently formats every output as 1.23E4 and does not give feedback
    about the number of characters printed or the position of the point.
    Improving all of them could give another speed improvement.

--- include/cpp_mould/arguments/float.hpp
+++ include/cpp_mould/arguments/float.hpp
@@ -4,6 +4,7 @@
 #include <cstdio>
 
 #include <double-conversion/double-conversion.h>
+#include <ryu/ryu.h>
 
 #include "../format.hpp"
 
@@ -22,18 +23,17 @@ namespace mould {
     char buffer[100];
 
     auto format = formatter.format();
-    auto& converter = DoubleToStringConverter::EcmaScriptConverter();
-    StringBuilder builder{buffer, 100};
+    char* result_buffer = buffer;
 
     if(format.sign == internal::Sign::Always && value >= 0)
-      builder.AddCharacter('+'); // Add the sign
+      *result_buffer++ = '+'; // Add the sign
     else if(format.sign == internal::Sign::Pad && value >= 0)
-      builder.AddCharacter(' '); // Add the sign
+      *result_buffer++ = ' '; // Add the sign
 
-    if(!converter.ToShortest(value, &builder))
-      return FormattingResult::Error;
-
-    unsigned length = builder.position();
+    for(auto rest = result_buffer; rest < buffer + 100; rest++)
+      *rest = '\0';
+    d2s_buffered(value, result_buffer);
+    unsigned length = std::strlen(buffer);
 
     const auto important_buffer = std::string_view{buffer, length};
     formatter.append(important_buffer);
@@ -46,19 +46,30 @@ namespace mould {
     char buffer[100];
 
     auto format = formatter.format();
-    auto& converter = DoubleToStringConverter::EcmaScriptConverter();
-    StringBuilder builder{buffer, 100};
+    char* result_buffer = buffer;
 
     if(format.sign == internal::Sign::Always && value >= 0)
-      builder.AddCharacter('+'); // Add the sign
+      *result_buffer++ = '+'; // Add the sign
     else if(format.sign == internal::Sign::Pad && value >= 0)
-      builder.AddCharacter(' '); // Add the sign
+      *result_buffer++ = ' '; // Add the sign
 
-    int fixed_count = format.has_precision ? format.precision : 6;
-    if(!converter.ToFixed(value, fixed_count, &builder))
-      return FormattingResult::Error;
+    for(auto rest = result_buffer; rest < buffer + 100; rest++)
+      *rest = '\0';
+    d2s_buffered(value, result_buffer);
 
-    unsigned length = builder.position();
+    unsigned length = std::strlen(buffer);
+
+    auto dot = std::find(buffer, buffer + length, '.');
+    if(dot == buffer + length) {
+      *dot = '.';
+      length++;
+    }
+
+    unsigned used_precision = length - (dot - buffer) - 1;
+    unsigned fixed_count = format.has_precision ? format.precision : 6;
+    for(auto fill = used_precision; fill < fixed_count; fill++) {
+      buffer[length++] = '0';
+    }
 
     const auto important_buffer = std::string_view{buffer, length};
     formatter.append(important_buffer);
